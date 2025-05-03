d0d3e5ab36e5b187ffe81dcf717d3901e68318b3
Archana0807 <archana.potharaju08@gmail.com>
2025-05-02 19:22:57 -0500

    Update db.php

--- db.php
+++ db.php
@@ -1,5 +1,5 @@
 <?php
-    $con = mysqli_connect("127.0.0.1","root","","organicfoodfinder");
+   $con = mysqli_connect("g3v9lgqa8h5nq05o.cbetxkdyhwsb.us-east-1.rds.amazonaws.com","pj6ksu0rkstkng7n","vngxsvu49lgkzsv0","ph4mtn2jzikxenid");
     // Check connection
     if (mysqli_connect_errno()){
         echo "Failed to connect to MySQL: " . mysqli_connect_error();
